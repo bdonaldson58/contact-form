5c00136bc1068d76abe08139dc8a6314c3dabf28
Brendan <bdonaldson58@gmail.com>
2017-08-10 16:37:21 -0500

    Cleaned up PHP and split files

---
+++ inc/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet"
+    href="https://bootswatch.com/cosmo/bootstrap.min.css">
+    <title>Contact Us</title>
+</head>
+<body>  
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="index.php">My Website</a>>
+        </div>
+      </div>
+    </nav>
